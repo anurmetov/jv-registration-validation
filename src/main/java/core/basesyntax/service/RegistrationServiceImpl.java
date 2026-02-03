148e3a1a7c4dd87e0075a4588b2c723e30cda78b
Dein <deine.email@example.com>
2026-02-03 18:37:24 +0100

    - implemented full logic

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -11,19 +11,45 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) throws InvalidDataException {
+        if (user.getLogin() == null) {
+            throw new InvalidDataException("Null login is not allowed");
+        }
+
+        if (user.getPassword() == null) {
+            throw new InvalidDataException("Null password is not allowed");
+        }
+
+        if (user.getPassword().isEmpty()) {
+            throw new InvalidDataException("Empty password is not allowed");
+
+        }
+
+        if (user.getLogin().isEmpty()) {
+            throw new InvalidDataException("Null login is not allowed");
+        }
+
         if (!Storage.people.contains(storageDao.get(user.getLogin()))) {
             if (user.getLogin().length() >= 6) {
 
                 if (user.getPassword().length() >= 6) {
-                    storageDao.add(user);
-                    return user;
+
+                    if (user.getAge() >= 18) {
+                        storageDao.add(user);
+                        return user;
+                    } else {
+                        throw new InvalidDataException("Age should be over 18");
+                    }
+
                 } else {
-                    throw new InvalidDataException("User password is too short! Min. 6 characters!");
+                    throw new InvalidDataException("User password is too short! "
+                            + "Min. 6 characters!");
 
                 }
+
             } else {
                 throw new InvalidDataException("User login is too short! Min. 6 characters!");
             }
+
         } else {
             throw new InvalidDataException("User exists in the storage!");
         }
