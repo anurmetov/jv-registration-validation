f9fb8a2d8e04733e1516c72955930adfdabc5b87
artem <artemnurmetow@gmail.com>
2026-02-09 21:24:46 +0100

    - deleted unused Class

--- src/main/java/core/basesyntax/exception/InvalidUserRegistration.java
+++
@@ -1,7 +0,0 @@
-package core.basesyntax.exception;
-
-public class InvalidUserRegistration extends RuntimeException {
-    public InvalidUserRegistration(String message) {
-        super(message);
-    }
-}
